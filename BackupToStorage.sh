e7974ccd663e117abcd699011be42edd527248bd
faraz <rezaei.abdolhadi@gmail.com>
2024-06-20 09:49:41 +0200

    check if cli is installed or not

--- BackupToStorage.sh
+++ BackupToStorage.sh
@@ -23,4 +23,11 @@ case $1 in
  ;;
 esac
 
-echo $path
+echo "PATH: $path"
+#checks if the aws command is available in the system's PATH and if it is executable. If the aws command is found, command -v aws would normally 
+#print the path to the aws executable. However, since the output is redirected to /dev/null, no output is produced.
+# Check if AWS CLI is installed
+if ! command -v aws &> /dev/null; then
+  echo "AWS CLI not found, please install it."
+  exit 1
+fi
